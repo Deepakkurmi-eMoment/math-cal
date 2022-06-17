9f6f279f5d398730fb4cd419a12964980dec0b13
Deepakkurmi-eMoment <deepak.kurmi@emoment.in>
2022-06-17 13:18:09 +0530

    changes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-export const Addition = (num1, num2) => {
+const Addition = (num1, num2) => {
   return num1 + num2;
 };
 
-export const Subtract = (num1, num2) => {
+const Subtract = (num1, num2) => {
   return num1 - num2;
 };
 
-export const Multiplay = (num1, num2) => {
+const Multiplay = (num1, num2) => {
   return num1 * num2;
 };
 
-export const Division = (num1, num2) => {
+const Division = (num1, num2) => {
   return num1 / num2;
+};
+
+module.exports = {
+  Addition,
+  Subtract,
+  Multiplay,
+  Division,
 };
